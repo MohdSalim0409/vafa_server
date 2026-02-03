bb8a20e18a791889766825347f67f7274b7a01af
MohdSalim0409 <mohdsalim0409@gmail.com>
2026-02-03 15:56:52 +0530

    adduser

--- server.js
+++ server.js
@@ -52,3 +52,19 @@ app.delete("/api/users/:id", async (req, res) => {
     }
 });
 
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// Create User
+
+
+app.post("/api/users", async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
