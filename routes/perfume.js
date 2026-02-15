a8e878fcc03e0e985bb760d5417233fe8ece490a
MohdSalim0409 <mohdsalim0409@gmail.com>
2026-02-15 16:25:06 +0530

    Refactor code structure for improved readability and maintainability

--- routes/perfume.js
+++ routes/perfume.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const router = express.Router();
-const PerfumeInventory = require("../models/PerfumeInventory");
+const PerfumeInventory = require("../models/perfumeInventory");
+const PerfumeMaster = require("../models/perfumeMaster");
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
 // Fetch perfumes list to show in store front page
 
 router.get("/", async (req, res) => {
-
     try {
-        const stock = await PerfumeInventory.find().populate("perfume");
-        res.json(stock);
+        const data = await PerfumeInventory.aggregate([
+            {
+                $lookup: {
+                    from: "perfumemasters",
+                    localField: "perfume",
+                    foreignField: "_id",
+                    as: "perfume"
+                }
+            },
+            { $unwind: "$perfume" },
+
+            {
+                $group: {
+                    _id: "$perfume._id",
+                    perfume: { $first: "$perfume" },
+                    variants: {
+                        $push: {
+                            inventoryId: "$_id",
+                            size: "$size",
+                            price: "$sellingPrice",
+                            quantity: "$quantity",
+                            status: "$status"
+                        }
+                    }
+                }
+            }
+        ]);
+
+        res.json(data);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(500).json(err);
     }
 });
+
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
